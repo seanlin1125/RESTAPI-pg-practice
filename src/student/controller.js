b931719f06b22dcdb969a8cd702667fa479376ce
seanlin1125 <sean821125@gmail.com>
2023-03-25 16:23:27 +0800

    feat: add addStudent route

--- src/student/controller.js
+++ src/student/controller.js
@@ -15,6 +15,24 @@ const studentController = {
       res.status(200).json(results.rows)
     })
   },
+  addStudent: (req, res) => {
+    const { name, email, age, dob } = req.body
+
+    pool.query(queries.checkEmailExists, [email], (error, results) => {
+      if (results.rows.length) {
+        res.send('Email already exists.')
+      }
+      res.status(200).json(results.rows)
+    })
+    pool.query(
+      queries.addStudent,
+      [name, email, age, dob],
+      (error, results) => {
+        if (error) throw error
+        res.status(201).send('Students created successfully!')
+      }
+    )
+  },
 }
 
 module.exports = studentController
--- src/student/routes.js
+++ src/student/routes.js
@@ -4,6 +4,7 @@ const studentController = require('./controller')
 const router = express.Router()
 
 router.get('/', studentController.getStudents)
+router.post('/', studentController.addStudent)
 router.get('/:id', studentController.getStudentById)
 
 module.exports = router
